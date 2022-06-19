aae9a8f3720f4a0dd999522856c6cd2a596fea2f
Nik <particlenik108@protonmail.com>
2022-06-19 03:43:13 +0200

    added comment

--- viz_tools.R
+++ viz_tools.R
@@ -54,7 +54,7 @@ long_form <- function(df){
 #-------------------------------------------------------------------------------
 
 
-
+# This may not be as efficient but its a start 
 
 # Heat map plots 
 # this take a dataframe in LONG FORM AS INPUT 
