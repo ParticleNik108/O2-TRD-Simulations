f0b341aff063e0ca777244b8ea73c225fd6318de
NikMSI <particlenik108@protonmail.com>
2022-06-23 20:46:23 -0700

    Code to plot a sample of signals

---
+++ plot_sample_sigs.R
@@ -0,0 +1,58 @@
+
+
+# Functions fot sampling and plotting trd signals 
+
+# They take in as input the reshaped data ready to be fed to a cnn ie. 4x30 arrays 
+
+library(ggplot2)
+library(gridExtra)
+library(ggpubr)
+library(cowplot)
+
+
+
+sample_signals <- function(df, nb = 6) {
+  
+  samps <- sample(1:dim(df)[1], size = nb)
+  
+  p1 <- df[samps[1],,] %>% melt() %>% ggplot(aes(x=Var2, y=Var1, fill=value)) + geom_tile() + 
+    scale_fill_viridis_c() +
+    labs(x="Timebin", y="Pads") + theme_bw() + 
+    theme(axis.text.y = element_blank() ) 
+  
+  p2 <- df[samps[2],,] %>% melt() %>% ggplot(aes(x=Var2, y=Var1, fill=value)) + geom_tile() + 
+    scale_fill_viridis_c() +
+    labs(x="Timebin", y="Pads") + theme_bw() + 
+    theme(axis.text.y = element_blank() ) 
+  
+  p3 <- df[samps[3],,] %>% melt() %>% ggplot(aes(x=Var2, y=Var1, fill=value)) + geom_tile() + 
+    scale_fill_viridis_c() +
+    labs(x="Timebin", y="Pads") + theme_bw() + 
+    theme(axis.text.y = element_blank() ) 
+  
+  p4 <- df[samps[4],,] %>% melt() %>% ggplot(aes(x=Var2, y=Var1, fill=value)) + geom_tile() + 
+    scale_fill_viridis_c() +
+    labs(x="Timebin", y="Pads") + theme_bw() + 
+    theme(axis.text.y = element_blank() ) 
+  
+  p5 <- df[samps[5],,] %>% melt() %>% ggplot(aes(x=Var2, y=Var1, fill=value)) + geom_tile() + 
+    scale_fill_viridis_c() +
+    labs(x="Timebin", y="Pads") + theme_bw() + 
+    theme(axis.text.y = element_blank() ) 
+  
+  p6 <- df[samps[6],,] %>% melt() %>% ggplot(aes(x=Var2, y=Var1, fill=value)) + geom_tile() + 
+    scale_fill_viridis_c() +
+    labs(x="Timebin", y="Pads") + theme_bw() + 
+    theme(axis.text.y = element_blank() ) 
+  
+  
+  
+  plotgrids <- grid.arrange(p1, p2, p3, p4, ncol=2)
+  
+  #plotgrids <- plot_grid(plotlist = c(p1,p2, p3, p4, p5, p6), ncol = 2)
+  
+  
+  return(plotgrids)
+  
+}
+  
